9d2aa9321f88d0bbffba0065200828470241702e
xss5813 <xss5813@163.com>
2018-08-31 09:42:29 +0800

    @！

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Pro } from '@ionic/pro';
 
 /**
  * @description 使用懒加载，不需要显示引入模块，引用组件的地方使用名称字符串代替
@@ -21,9 +22,26 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      //this.performAutomaticUpdate();
     });
 
   }
   
+  async performAutomaticUpdate() {
+    try {
+      const currentVersion = await Pro.deploy.getCurrentVersion();
+      const resp = await Pro.deploy.sync({updateMethod: 'auto'});
+      if (currentVersion.versionId !== resp.versionId){
+        // We found an update, and are in process of redirecting you since you put auto!
+      }else{
+        // No update available
+      }
+    } catch (err) {
+      // We encountered an error.
+      // Here's how we would log it to Ionic Pro Monitoring while also catching:
+  
+       Pro.monitoring.exception(err);
+    }
+  }
   
 }
