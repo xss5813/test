a71247aa486e864dc87f8bc4a80c289c8f2445f4
xss5813 <xss5813@163.com>
2018-09-06 11:13:23 +0800

    @

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,9 @@
-import { Component, Renderer2, Renderer, ViewChild, NgZone } from '@angular/core';
+import { Component, Renderer2, Renderer, ViewChild, NgZone, OnInit } from '@angular/core';
 import { NavController, ModalController, Content } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular';
+
+
+
 
 @IonicPage()
 
@@ -8,9 +11,12 @@ import { IonicPage } from 'ionic-angular';
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
-export class ContactPage {
+export class ContactPage implements OnInit{
+  //key为string , value为number
+ map: { [key: string]: number} = {};
+ items:string[] = ['菜单1','菜单2','菜单3','菜单4',];
   @ViewChild(Content) content: Content;
-  private _testSegment: string;
+  private _testSegment: string="菜单2";
 
     get testSegment(): string {
         return this._testSegment;
@@ -18,16 +24,31 @@ export class ContactPage {
 
     set testSegment(newName: string) {
         
+      this._testSegment = newName;
       document.getElementById(newName).scrollIntoView(true);
-      
       console.log(newName);
     }
   myParam = 'xss';
+
   constructor(public navCtrl: NavController, public modalCtrl: ModalController,public zone: NgZone) {
 
   }
+  ngAfterViewInit(){
+    for(let i=0;i<this.items.length;i++){
+
+      this.map[this.items[i]] = document.getElementById(this.items[i]).offsetTop;
+    }
+  }
+  ngOnInit(){
+
+    
+  }
   scrollHandler(event) {
     console.log(`ScrollEvent: ${event}`)
+
+    let name =this.items.find((value)=>this.map[value]==event.scrollTop);
+    if(name)
+    this.testSegment=name;
     this.zone.run(()=>{
       // since scrollAmount is data-binded,
       // the update needs to happen in zone
